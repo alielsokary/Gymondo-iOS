93e3f281761a3e77133aeed00874ee92851f1cd1
Ali Elsokary <ali.elsokary.w@gmail.com>
2023-08-22 00:12:45 +0300

    Add new macOS Framework

---
+++ Gymondo/Gymondo/Gymondo.h
@@ -0,0 +1,18 @@
+//
+//  Gymondo.h
+//  Gymondo
+//
+//  Created by Ali Elsokary on 22/08/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Gymondo.
+FOUNDATION_EXPORT double GymondoVersionNumber;
+
+//! Project version string for Gymondo.
+FOUNDATION_EXPORT const unsigned char GymondoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gymondo/PublicHeader.h>
+
+
