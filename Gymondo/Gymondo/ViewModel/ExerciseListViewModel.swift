d921924c52ce139f4afc2869ede3028574243c8d
Ali Elsokary <ali.elsokary.w@gmail.com>
2023-08-24 16:19:18 +0300

    Replace `ExerciseListViewControllerTests.ViewModelSpy` with production type `ExerciseListViewModelLogic` protocol abstraction

---
+++ Gymondo/Gymondo/ViewModel/ExerciseListViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  ExerciseListViewModel.swift
+//  Gymondo
+//
+//  Created by Ali Elsokary on 24/08/2023.
+//
+
+import Foundation
+
+protocol ExerciseListViewModelLogic {
+    func start()
+}
+
+class ExerciseListViewModel: ExerciseListViewModelLogic {
+
+    func start() {
+
+    }
+}
--- Gymondo/GymondoiOSTests/ExerciseListViewControllerTests.swift
+++ Gymondo/GymondoiOSTests/ExerciseListViewControllerTests.swift
@@ -7,47 +7,49 @@
 
 import XCTest
 import UIKit
+@testable import Gymondo
 
 final class ExerciseListViewController: UIViewController {
-    private var viewModel: ExerciseListViewControllerTests.ViewModelSpy?
-    
-    convenience init(viewModel: ExerciseListViewControllerTests.ViewModelSpy) {
+    private var viewModel: ExerciseListViewModelLogic?
+
+    convenience init(viewModel: ExerciseListViewModelLogic) {
         self.init()
         self.viewModel = viewModel
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        viewModel?.load()
+
+        viewModel?.start()
     }
-    
+
 }
 
 final class ExerciseListViewControllerTests: XCTestCase {
-    
+
     func test_init_doesNotLoadExerciseList() {
         let viewModel = ViewModelSpy()
         _ = ExerciseListViewController(viewModel: viewModel)
-        
+
         XCTAssertEqual(viewModel.loadCallCount, 0)
     }
-    
+
     func test_viewDidLoad_loadsExerciseList() {
         let viewModel = ViewModelSpy()
         let sut = ExerciseListViewController(viewModel: viewModel)
-        
+
         sut.loadViewIfNeeded()
-        
+
         XCTAssertEqual(viewModel.loadCallCount, 1)
     }
-    
+
     // MARK: - Helpers
-    
-    class ViewModelSpy {
+
+    class ViewModelSpy: ExerciseListViewModelLogic {
+
         private(set) var loadCallCount: Int = 0
-        
-        func load() {
+
+        func start() {
             loadCallCount += 1
         }
     }
