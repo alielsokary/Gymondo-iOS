b156513da2d925eaea781ef6b9e69f377fd4fd19
Ali Elsokary <ali.elsokary.w@gmail.com>
2023-08-25 19:29:15 +0300

    Add API Parameters and Encodable Extension helper

--- Gymondo/Gymondo/Models/ExerciseImages.swift
+++ Gymondo/Gymondo/Models/ExerciseImages.swift
@@ -5,4 +5,34 @@
 //  Created by Ali Elsokary on 25/08/2023.
 //
 
-import Foundation
+// MARK: - ExerciseImages
+public struct ExerciseImages: Codable {
+    public let results: [ImageItem]?
+}
+
+// MARK: - ImageItem
+public struct ImageItem: Codable {
+    public let id: Int?
+    public let uuid: String?
+    public let exerciseBase: Int?
+    public let exerciseBaseUUID: String?
+    public let image: String?
+    public let isMain: Bool?
+
+    public init(id: Int?, uuid: String?, exerciseBase: Int?, exerciseBaseUUID: String?, image: String?, isMain: Bool?) {
+        self.id = id
+        self.uuid = uuid
+        self.exerciseBase = exerciseBase
+        self.exerciseBaseUUID = exerciseBaseUUID
+        self.image = image
+        self.isMain = isMain
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case id, uuid
+        case exerciseBase = "exercise_base"
+        case exerciseBaseUUID = "exercise_base_uuid"
+        case image
+        case isMain = "is_main"
+    }
+}
