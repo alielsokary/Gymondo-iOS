198e0568e7764aab1e1a3cc0b07799fa379ea437
Ali Elsokary <ali.elsokary.w@gmail.com>
2023-08-25 19:31:39 +0300

    Update UI of ExerciseTableViewCell

--- Gymondo/GymondoiOS/Scenes/ExerciseList/View/Cell/ExerciseItemCell.swift
+++ Gymondo/GymondoiOS/Scenes/ExerciseList/View/Cell/ExerciseItemCell.swift
@@ -11,4 +11,18 @@ class ExerciseItemCell: UITableViewCell {
 
     @IBOutlet weak var exerciseImageView: UIImageView!
     @IBOutlet weak var exerciseNameLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        setupUI()
+    }
+
+    func setupUI() {
+        exerciseImageView.layer.cornerRadius = 8.0
+        exerciseImageView.clipsToBounds = true
+
+        exerciseImageView.layer.borderWidth = 1.0
+        exerciseImageView.layer.borderColor = UIColor.lightGray.cgColor
+    }
 }
